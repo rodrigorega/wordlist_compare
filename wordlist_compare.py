ea5110bcd6a8097128e83ac3536e9d9ca4042fc9
Rodrigo Rega <contacto@rodrigorega.es>
2014-09-13 02:38:05 +0200

    PEP8 compliant

--- wordlist_compare.py
+++ wordlist_compare.py
@@ -17,10 +17,10 @@ Output:
     - match_file
 
 Author: Rodrigo Rega <contacto@rodrigorega.es>
-License: CC-BY-SA 3.0 license (http://creativecommons.org/licenses/by/3.0/    
+License: CC-BY-SA 3.0 license (http://creativecommons.org/licenses/by/3.0/
 '''
 
-###################### CONFIGURATION START ###########################
+# ##################### CONFIGURATION START ###################################
 
 # CSV with leaked data. File in format: my@email[leak_file_split]password
 leak_file = 'Gmail.txt'
@@ -41,72 +41,76 @@ big_file_mode = False
 # Case sensitive is a little faster
 case_sensitive = True
 
-###################### CONFIGURATION END #############################
+# ##################### CONFIGURATION END #####################################
 
 import time
 
 contacts_file_len = sum(1 for line in open(contacts_file))
 
+
 def search_leak_file():
     '''
     Compare lines in 2 files
-    
+
     Return: None
     '''
     contacts_list = _read_file_to_memory(contacts_file)
-    leaks_list  = _read_file_to_memory(leak_file)
-    
+    leaks_list = _read_file_to_memory(leak_file)
+
     for i, contact in enumerate(contacts_list):
         contact = _converte_to_sensitive_if_configured(contact)
-        _print_job_status(i,contact)
-        t1=time.clock()
+        _print_job_status(i, contact)
+        t1 = time.clock()
 
         for leak in leaks_list:
             leak = _converte_to_sensitive_if_configured(leak)
-            
-            #if contact == leak.split(leak_file_split)[0]:
+
+            # if contact == leak.split(leak_file_split)[0]:
             if leak.startswith(contact):
                 _print_match_found(leak)
                 _add_to_match_file(leak)
         _print_job_time(t1)
 
+
 def search_leak_file_big_file():
     '''
     Compare lines in 2 files, version for big files (or for low memory RAM)
-    
+
     Return: None
     '''
     with open(contacts_file, 'rU') as contacts_list:
         for i, contact in enumerate(contacts_list):
             contact = _converte_to_sensitive_if_configured(contact)
-            _print_job_status(i,contact)
-            t1=time.clock()
+            _print_job_status(i, contact)
+            t1 = time.clock()
 
             with open(leak_file) as leaks_list:
                 for leak in leaks_list:
                     leak = _converte_to_sensitive_if_configured(leak)
 
-                    #if contact == leak.split(leak_file_split)[0]:
+                    # if contact == leak.split(leak_file_split)[0]:
                     if leak.startswith(contact):
                         _print_match_found(leak)
                         _add_to_match_file(leak)
             _print_job_time(t1)
 
+
 def _converte_to_sensitive_if_configured(one_string):
     '''
     Convert string to lower if configured
-    
+
     Return: same string or lowered
     '''
     if case_sensitive:
         return one_string.rstrip()
     else:
         return one_string.lower().rstrip()
-    
+
+
 def _read_file_to_memory(filename):
     '''
     Get file contents and store it in RAM
-    
+
     Return: contents of readed file
     '''
     try:
@@ -117,99 +121,112 @@ def _read_file_to_memory(filename):
         _print_exiting()
         exit(1)
 
+
 def _add_to_match_file(data):
     '''
     Append one line to otput file
-    
+
     Return: None
     '''
-    
+
     try:
         f = open(match_file, "a")
     except:
-        _print_file_open_error(filename)
+        _print_file_open_error(match_file)
         _print_exiting()
         exit(1)
-        
+
     try:
-        f.write("%s\n" %data)
+        f.write("%s\n" % data)
     except:
-        _print_file_open_error(filename)
+        _print_file_open_error(match_file)
         _print_exiting()
         exit(1)
-        
+
     f.close()
+
 
 def _print_file_open_error(filename):
     '''
     Show unable to open in console
-    
+
     Return: filename
     '''
     print "[!] Unable to open  %s " % filename
-    
+
+
 def _print_file_write_error(filename):
     '''
     Show write error in console
-    
+
     Return: filename
     '''
     print "[!] Unable to write to  %s " % filename
 
+
 def _print_exiting():
     '''
     Show exiting in console
-    
+
     Return: filename
     '''
     print "[*] Exiting"
 
+
 def _print_job_start():
     '''
     Show start message in console
-    
+
     Return: start time timer
     '''
-    t1=time.clock()
+    t1 = time.clock()
     print "[*] Starting...\n"
     return t1
+
 
 def _print_job_end(t0):
     '''
     Show end message in console
-    
+
     Return: None
     '''
     print("[*] Finish. Duration: %f seconds\n" % (time.clock()-t0))
-    
-def _print_job_status(contact_number,contact):
+
+
+def _print_job_status(contact_number, contact):
     '''
     Show status job info in console
-    
+
     Return: None
     '''
-    print "[*] Processing %d of %d: %s" % (contact_number+1, contacts_file_len, contact.split(leak_file_split)[0].lower())
-    
+    print "[*] Processing %d of %d: %s" % (contact_number + 1,
+                                           contacts_file_len,
+                                           contact.split(leak_file_split)
+                                           [0].lower())
+
+
 def _print_match_found(matched):
     '''
     Show matched job  in console
-    
+
     Return: None
     '''
     print ("[!] Email match in leaked file: %s" % (matched))
 
+
 def _print_job_time(t1):
     '''
     Show job duration time in console
-    
+
     Return: None
     '''
     print("[-] Search duration: %f seconds\n" % (time.clock()-t1))
-        
+
+
 if __name__ == "__main__":
     '''
     Main
-    
+
     Return: None
     '''
     t0 = _print_job_start()
